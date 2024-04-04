c31befd6a5f62b89e2c558094a0732797b57469b
Riceball LEE <snowyu.lee@gmail.com>
2024-04-04 17:01:20 +0800

    test: use fastify instead of http-server

--- test/server-setup.mjs
+++ test/server-setup.mjs
@@ -1,18 +1,33 @@
 import path from 'path'
-import { createServer } from 'http-server'
-import { findPort } from './test/find-port'
+// import { createServer } from 'http-server'
+import { fastify } from 'fastify';
+import { fastifyStatic } from '@fastify/static';
+import { findPort } from './find-port'
 
 let server
 export async function setup({provide}) {
   server = globalThis.__vitest_server__ || createServer({
     root: path.join(__dirname, 'res'),
+    // exposeHeadRoutes: false,
+    // exposeHeadRoute: false,
+    // wildcard: false, // without this the content-length should be always 0, already fixed in 7.0.3
+    setHeaders(res, path, stat) {
+      // console.log('🚀 ~ setHeaders ~ path:', path, res, stat)
+      // res.setHeader('X-content-length', stat.size)
+    },
+    // acceptRanges: true,
+    // prefix: '/res/',
   })
-  const port = await findPort('8088')
+  const port = await findPort('8089')
   const host = '127.0.0.1'
+  // const url = `http://${host}:${port}/res/`
   const url = `http://${host}:${port}/`
-  server.listen({port, host})
+  await server.listen({port, host, exposeHeadRoutes: false})
   server.url = url
+  // can not provide server for this is in another process
   provide('server-url', url)
+  console.log(server.printRoutes({ includeHooks: true, includeMeta: ['errorHandler'] }))
+
 }
 
 let teardownHappened = false
@@ -20,12 +35,16 @@ let teardownHappened = false
 export async function teardown() {
   if (teardownHappened) throw new Error('teardown called twice')
   teardownHappened = true
-  return new Promise((resolve) => {
-    console.log('Closing server...');
-    server.server.close(() => {
-      console.log('Server closed');
-      resolve();
-    })
-  })
+  console.log('Closing server...');
+  await server.close()
+  console.log('Server closed');
+}
 
+function createServer(options) {
+  const server = fastify(options)
+  options = { ...options }
+  if (options.root) {
+    server.register(fastifyStatic, options)
+  }
+  return server
 }
